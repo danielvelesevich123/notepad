ef1968214efe3aa23f47b15dc4ca649f4681e5b2
danielvelesevich123 <danielvelsevich@gmail.com>
2018-10-05 11:24:33 +0300

    methods overridden

--- task.rb
+++ task.rb
@@ -1,14 +1,28 @@
+require 'date'
+
 class Task < Post
   def initialize
     super
 
-    @dua_data = Time.now
+    @due_date = Time.now
   end
 
   def read_from_console
+    puts "Что нужно сделать?"
+    @text = STDIN.gets.chomp
+
+    puts "К какому числу? Укажите дату в формате дд.мм.гггг, например 12.05.2020"
+    input = STDIN.gets.chomp
+
+    @due_date = Date.parse(input)
   end
 
   def to_strings
+    time_string = "Создано: #{@created_at.strftime("@Y-@m-@d_@H-@M-@S")} \n\r \n\r"
+
+    deadline = "Крайний день: #{@due_date}"
+
+    return [deadline, @text, time_string]
   end
 
 end
